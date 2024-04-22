1e1c7ef6d996863e32401e161b6c3699deef42f4
tinyfacefn <tinyfacefn@DESKTOP-PS8HP1A>
2024-04-22 11:02:58 -0400

    add

---
+++ letAndConst.js
@@ -0,0 +1,13 @@
+//ex1
+let pi = 3.14
+// - What is the difference between ***var*** and ***let***?
+//var assigns the value to the webpage let assigns the value inside the block{}
+
+// - What is the difference between **var** and ***const***?
+//similar to let  but const stops you from re assigning the variable
+
+// - What is the difference between **let** and **const**?
+//let you can re assign the variable to a new value but const you can not
+
+// - What is hoisting?
+// hoisting is where when declaring var the var x = part is "hoisted" and read before the rest of the code, without the value so var x = undefined at start 
